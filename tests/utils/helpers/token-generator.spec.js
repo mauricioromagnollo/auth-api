a080581c6e3f29b953a5eb0daa48243fd2e9f363
x0n4d0 <romagnolimauricio@gmail.com>
2023-01-05 00:29:46 -0300

    feat: ensure TokenGenerator returns null if jwt returns null

---
+++ tests/utils/helpers/token-generator.spec.js
@@ -0,0 +1,15 @@
+class TokenGenerator {
+  async generate (id) {
+    return null
+  }
+}
+
+describe('Token Generator', () => {
+  test('should return true if jwt returns null', async () => {
+    const sut = new TokenGenerator()
+
+    const token = await sut.generate('any_id')
+
+    expect(token).toBeNull()
+  })
+})
