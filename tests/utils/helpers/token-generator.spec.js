c11ece26a1e9474b3507aa9c31c17e8d0429cdcc
x0n4d0 <romagnolimauricio@gmail.com>
2023-01-05 00:42:21 -0300

    test: should call jwt with correct values

--- tests/utils/helpers/token-generator.spec.js
+++ tests/utils/helpers/token-generator.spec.js
@@ -1,14 +1,22 @@
 const jwt = require('jsonwebtoken')
 
 class TokenGenerator {
-  async generate (id) {
-    return jwt.sign(id, 'secret')
+  constructor (secret) {
+    this.secret = secret
   }
+
+  async generate (id) {
+    return jwt.sign(id, this.secret)
+  }
+}
+
+const makeSut = () => {
+  return new TokenGenerator('any_secret')
 }
 
 describe('Token Generator', () => {
   test('should return true if jwt returns null', async () => {
-    const sut = new TokenGenerator()
+    const sut = makeSut()
 
     jwt.token = null
     const token = await sut.generate('any_id')
@@ -17,10 +25,19 @@ describe('Token Generator', () => {
   })
 
   test('should return true if jwt returns null', async () => {
-    const sut = new TokenGenerator()
+    const sut = makeSut()
 
     const token = await sut.generate('any_id')
 
     expect(token).toBe(jwt.token)
+  })
+
+  test('should call jwt with correct values', async () => {
+    const sut = makeSut()
+
+    await sut.generate('any_id')
+
+    expect(jwt.id).toBe('any_id')
+    expect(jwt.secret).toBe(sut.secret)
   })
 })
