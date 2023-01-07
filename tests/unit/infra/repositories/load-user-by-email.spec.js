14382e06faaed6c7a22651567488acd76d20c554
x0n4d0 <romagnolimauricio@gmail.com>
2023-01-07 11:55:15 -0300

    test: move sut to factory method

--- tests/unit/infra/repositories/load-user-by-email.spec.js
+++ tests/unit/infra/repositories/load-user-by-email.spec.js
@@ -1,11 +1,52 @@
 const { LoadUserByEmailRepository } = require('../../../../src/infra/repositories')
 
-describe('LoadUserByEmailRepository', () => {
-  test('should return null if no user is found', async () => {
-    const sut = new LoadUserByEmailRepository()
+const makeSut = () => {
+  const userModelSpy = makeUserModelSpy()
+  const sut = new LoadUserByEmailRepository(userModelSpy)
 
-    const user = await sut.exec('invalid_email@mail.com')
+  return {
+    sut,
+    userModelSpy
+  }
+}
+
+const makeUserModelSpy = () => {
+  return {
+    users: [
+      { email: '' }
+    ],
+    findOne ({ email }) {
+      const user = this.users.find(user => user.email === email)
+      return user ?? null
+    },
+    insertOne ({ email }) {
+      const user = this.findOne({ email })
+
+      if (user) {
+        return null
+      }
+
+      return this.users.push({ email })
+    }
+  }
+}
+
+describe('LoadUserByEmailRepository', () => {
+  test('should return null if no user is not found', async () => {
+    const { sut } = makeSut()
+
+    const user = await sut.exec('not_found_email@mail.com')
 
     expect(user).toBeNull()
+  })
+
+  test('should return an user if user is found', async () => {
+    const { sut, userModelSpy } = makeSut()
+    const email = 'valid_email@mail.com'
+    userModelSpy.insertOne({ email })
+
+    const user = await sut.exec(email)
+
+    expect(user.email).toBe(email)
   })
 })
