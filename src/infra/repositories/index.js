111d67f4e7a564cdf34d739425da2f71facc21b7
x0n4d0 <romagnolimauricio@gmail.com>
2023-01-15 21:00:38 -0300

    feat: add UpdateAccessToken repository

--- src/infra/repositories/index.js
+++ src/infra/repositories/index.js
@@ -1,5 +1,7 @@
 const { LoadUserByEmailRepository } = require('./load-user-by-email')
+const { UpdateAccessTokenRepository } = require('./update-access-token')
 
 module.exports = {
-  LoadUserByEmailRepository
+  LoadUserByEmailRepository,
+  UpdateAccessTokenRepository
 }
---
+++ src/infra/repositories/update-access-token.js
@@ -0,0 +1,29 @@
+const { MissingParamError } = require('../../utils/errors')
+
+class UpdateAccessTokenRepository {
+  constructor (userModel) {
+    this.userModel = userModel
+  }
+
+  async exec (userId, accessToken) {
+    if (!userId) {
+      throw new MissingParamError('userId')
+    }
+
+    if (!accessToken) {
+      throw new MissingParamError('accessToken')
+    }
+
+    await this.userModel.updateOne({
+      _id: userId
+    }, {
+      $set: {
+        accessToken
+      }
+    })
+  }
+}
+
+module.exports = {
+  UpdateAccessTokenRepository
+}
