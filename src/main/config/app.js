3e90e0a86d13d0b7e901dc39e96c6924b50f7f58
x0n4d0 <romagnolimauricio@gmail.com>
2023-01-15 23:55:05 -0300

    chore: setup setup env variables

--- src/main/config/app.js
+++ src/main/config/app.js
@@ -3,6 +3,14 @@ const app = express()
 
 const { setupApp } = require('./setup')
 
+const NODE_ENV = process.env.NODE_ENV
+
+if (NODE_ENV !== 'prd') {
+  require('dotenv').config({
+    path: `.env.${NODE_ENV}`
+  })
+}
+
 setupApp(app)
 
 module.exports = {
---
+++ src/main/config/env.js
@@ -0,0 +1,15 @@
+const Env = {
+  NODE_ENV: process.env.NODE_ENV || 'dev',
+  PORT: Number(process.env.PORT) || 3000,
+  DATABASE_USERNAME: process.env.DATABASE_USERNAME || 'root',
+  DATABASE_PASSWORD: process.env.DATABASE_PASSWORD || 'root',
+  DATABASE_NAME: process.env.DATABASE_NAME || 'auth_api',
+  DATABASE_PORT: Number(process.env.DATABASE_PORT) || 27017,
+  DATABASE_HOST: process.env.DATABASE_HOST || 'localhost',
+  DATABASE_CONNECTION_URL: process.env.DATABASE_CONNECTION_URL ||
+    `mongodb://${this.DATABASE_USERNAME}:${this.DATABASE_PASSWORD}$@${this.DATABASE_HOST}:${this.DATABASE_PORT}/${this.DATABASE_NAME}?authSource=admin`
+}
+
+module.exports = {
+  Env
+}
